314d496b0781ba8b0bd1c72199043ab27e6ccf66
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2021-05-05 11:17:54 +0900

    xfmt: リファクタ

--- src/bin/xfmt.rs
+++ src/bin/xfmt.rs
@@ -22,6 +22,15 @@ fn main() {
             .default_value("\t")
         )
         .arg(
+            Arg::with_name("MAX_WIDTH")
+            .short("w")
+            .long("max-width")
+            .value_name("MAX_WIDTH")
+            .help("max width by column")
+            .takes_value(true)
+            .default_value("100")
+        )
+        .arg(
             Arg::with_name("INPUT_FILE")
             .value_name("FILE_NAME")
             .help("input file name")
@@ -31,59 +40,74 @@ fn main() {
     let delimiter: &str = matches
         .value_of("DELIMITER")
         .unwrap();
+    let max_width: usize = matches // TODO: 数値パースのエラーハンドリング
+        .value_of("MAX_WIDTH")
+        .map(|v| v.parse::<usize>().ok().unwrap())
+        .unwrap();
     let input_file = matches
         .value_of("INPUT_FILE");
     let reader = utils::reader(input_file);
     let mut writer = utils::writer(None);
-
-    let mut buffer: Vec<Vec<String>> = vec![];
-    let mut padding_util = None;
+    let mut line_buffer: Vec<String> = vec![];
+    let mut formatter = None;
     for (ln, line) in reader.lines().enumerate() {
         let line: String = line.ok().unwrap();
-        // TODO: 全部Stringにしてると遅い
-        let cols: Vec<String> = line.split(delimiter).map(|e| e.to_string()).collect();
         if ln < 100 {
-            buffer.push(cols);
+            line_buffer.push(line);
         } else if ln == 100 {
-            padding_util = Some(PaddingUtil::new(&buffer));
-            if let Some(ref padding_util) = padding_util {
-                for bcols in buffer.iter() {
-                    if writer.write(padding_util.format(&bcols).as_bytes()).is_err() == true {
-                        process::exit(0);
-                    }
+            let rows = line_buffer
+                .iter()
+                .map(|l| l.split(delimiter).collect::<Vec<&str>>())
+                .collect::<Vec<Vec<&str>>>();
+            formatter = Some(XsvFormatter::new(&rows, max_width));
+            if let Some(ref formatter) = formatter {
+                for cols in rows.iter() {
+                    write_line(&mut writer, formatter.format(&cols));
                 }
-                if writer.write(padding_util.format(&cols).as_bytes()).is_err() == true {
-                    process::exit(0);
-                }
+                let cols: Vec<&str> = line.split(delimiter).collect();
+                write_line(&mut writer, formatter.format(&cols));
             }
         } else {
-            if let Some(ref padding_util) = padding_util {
-                if writer.write(padding_util.format(&cols).as_bytes()).is_err() == true {
-                    process::exit(0);
-                }
+            let cols: Vec<&str> = line.split(delimiter).collect();
+            if let Some(ref formatter) = formatter {
+                write_line(&mut writer, formatter.format(&cols));
             }
         }
     }
-    if padding_util.is_none() {
-        padding_util = Some(PaddingUtil::new(&buffer));
-        if let Some(ref padding_util) = padding_util {
-            for bcols in buffer.iter() {
-                if writer.write(padding_util.format(&bcols).as_bytes()).is_err() == true {
-                    process::exit(0);
-                }
+    // 入力が100行以下だったときにline_bufferを出力する
+    if formatter.is_none() {
+        let rows = line_buffer
+            .iter()
+            .map(|l| l.split(delimiter).collect::<Vec<&str>>())
+            .collect::<Vec<Vec<&str>>>();
+        formatter = Some(XsvFormatter::new(&rows, max_width));
+        if let Some(ref formatter) = formatter {
+            for cols in rows.iter() {
+                write_line(&mut writer, formatter.format(&cols));
             }
         }
     }
+}
+
+fn write_line(writer: &mut dyn Write, line: String) {
+    if writer.write(line.as_bytes()).is_err() == true {
+        process::exit(0);
+    }
+    if writer.write("\n".as_bytes()).is_err() == true {
+        process::exit(0);
+    }
+
 }
 
 #[derive(Debug)]
-struct PaddingUtil {
-    width_list: Vec<usize>,
-    col_length: usize,
+struct XsvFormatter {
+    width_list: Vec<usize>, // 各カラムの表示幅リスト
+    col_length: usize,      // ヘッダ行のカラム数
+    max_width: usize,       // カラムの最大幅
 }
 
-impl PaddingUtil {
-    fn new(rows: &[Vec<String>]) -> Self {
+impl XsvFormatter {
+    fn new(rows: &[Vec<&str>], max_width: usize) -> Self {
         let col_length = if let Some(cols) = rows.get(0) { cols.len() } else { 0 };
         let mut width_list = vec![0; col_length];
         for i in 0..col_length {
@@ -91,21 +115,21 @@ impl PaddingUtil {
             let max_value_length = rows
                 .iter()
                 .map(|cols| {
-                    let value: &str = if let Some(v) = cols.get(i) {v} else {&""};
+                    let value: &str = cols.get(i).unwrap_or(&"");
                     let width =  UnicodeWidthStr::width(value) + 1;
-                    if width > 100 {100} else {width}
+                    if width > max_width {max_width} else {width}
                 })
                 .max()
                 .unwrap();
             width_list[i] = max_value_length;
         }
-        PaddingUtil { width_list, col_length }
+        XsvFormatter { width_list, col_length, max_width }
     }
 
-    fn format(&self, cols: &[String]) -> String {
+    fn format(&self, cols: &[&str]) -> String {
         let mut ret = String::new();
         for (i, &width) in self.width_list.iter().enumerate() {
-            let value: &str = if let Some(v) = cols.get(i) {v} else {&""};
+            let value: &str = cols.get(i).unwrap_or(&"");
             let value_len =  UnicodeWidthStr::width(value);
             if (i + 1) == self.col_length {
                 // 最終カラムはすべて表示
@@ -131,7 +155,6 @@ impl PaddingUtil {
                 write!(ret, " ").unwrap();
             }
         }
-        write!(ret, "\n").unwrap();
         return ret;
     }
 }
